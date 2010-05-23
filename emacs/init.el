ea9b60fcabf8b81efb09f4f48e811b7782a0ce5d
Jens Petersen <juhpetersen gmail com>
2010-05-23 19:38:48 +1000

    merge in more pieces
    - frame-title-format
    - setup (e)shell
    - use vlgothic for ja
    - show diary if present
    - use require not load
    - use lisp/ for personal elisp
    - add dired-diff and dired-x
    - config for ediff
    - setup w3m
    - set EDITOR to emacsclient

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,10 @@
+;;; .emacs.d/init.el
+
 (setq column-number-mode t
       custom-file "~/.emacs.d/custom.el"
       diff-switches "-u"
       enable-recursive-minibuffers t
+      frame-title-format "%b - emacs"
       help-window-select t
       indent-tabs-mode nil
       inhibit-startup-screen t
@@ -13,6 +16,7 @@
 (load custom-file)
 
 (global-set-key "\C-xw" 'write-region)
+(global-set-key "\C-ce" 'shell)
 (global-set-key "\C-cf" 'find-function-other-window)
 (global-set-key "\C-cj" 'compile)
 (global-set-key "\C-ck" 'find-function-on-key)
@@ -23,24 +27,93 @@
 (global-set-key [M-left] 'backward-sexp)
 (global-set-key [M-right] 'forward-sexp)
 
-;;; personal elisp dir
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp"))
+;; ja font
+(set-fontset-font t 'japanese-jisx0208 "VL ゴシック")
+
+;;; personal lisp dir
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
 ;;; browse-kill-ring
 (browse-kill-ring-default-keybindings)
 
+;;; diary
+(if (file-readable-p "~/diary")
+    (diary 7))
+
 ;;; dired
 (setq dired-keep-marker-rename ?R)
+
+;; dired-diff
+(autoload 'dired-diff "dired-diff" nil t)
+(autoload 'dired-backup-diff "dired-diff" nil t)
+(autoload 'dired-ediff "dired-diff" nil t)
+(require 'dired)
+(define-key dired-mode-map "=" nil)
+(define-key dired-mode-map "=d" 'dired-diff)
+(define-key dired-mode-map "=b" 'dired-backup-diff)
+(define-key dired-mode-map "=m" 'dired-emerge)
+(define-key dired-mode-map "=a" 'dired-emerge-with-ancestor)
+(define-key dired-mode-map "=e" 'dired-ediff)
+(define-key dired-mode-map "=p" 'dired-epatch)
+
+;;; dired-x
+(setq dired-guess-shell-alist-user 
+      '(("\\.pdf$" "evince")
+	("\\.t\\(ar\\.bz2\\|bz\\)$" "tar jxvf" "tar jtvf")
+	))
+(require 'dired-x)
+
+;;; ediff
+(setq ediff-window-setup-function 'ediff-setup-windows-plain
+      ediff-split-window-function 'split-window-horizontally)
+
+;;; eshell
+(setq eshell-buffer-shorthand t
+      eshell-hist-ignoredups t
+      eshell-term-name "eterm-color")
+(defun juhp-eshell-hook-fn ()
+  (define-key eshell-mode-map "\C-u" 'eshell-kill-input)
+  (define-key eshell-mode-map "\C-w" 'backward-kill-word)
+  ;(setq pcomplete-cycle-completions nil)
+  )
+;(add-hook 'eshell-mode-hook
+;	  'juhp-eshell-hook-fn)
+
+(defun eshell/less (&rest args)
+    "Invoke `view-file' on a file."
+    (while args
+      (view-file (pop args))))
 
 ;;; ffap
 (setq ffap-require-prefix t)
 (ffap-bindings)
 
 ;;; iswitchb
-(load "iswitchb")
+(require 'iswitchb)
 (iswitchb-default-keybindings)
 (defalias 'read-buffer 'iswitchb-read-buffer)
 
+;;; w3m
+(setq w3m-use-cookies t
+      w3m-key-binding 'info
+      w3m-default-display-inline-images t)
+;(setq browse-url-browser-function 'w3m-browse-url)
+(eval-after-load "w3m-search"
+            '(progn
+               (add-to-list 'w3m-search-engine-alist
+                            '("RH bugzilla bug no"
+                              "https://bugzilla.redhat.com/show_bug.cgi?id=%s"
+                              nil))
+               (add-to-list 'w3m-uri-replace-alist
+                            '("\\`bz:" w3m-search-uri-replace "RH bugzilla bug no"))))
 
+;; scala
+;(add-to-list 'load-path "~/usr/scala/misc/scala-tool-support/emacs/")
+;(require 'scala-mode-auto)
+
+;;; server
+(setenv "EDITOR" "emacsclient")
+(require 'server)
 (unless (server-running-p)
   (server-start))
+
