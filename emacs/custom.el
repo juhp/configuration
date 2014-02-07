d9586aa8e35baf636a3a6c486e4e94734af9803f
Jens Petersen <juhpetersen gmail com>
2014-02-07 16:53:10 +0900

    add emacs/custom.el

---
+++ emacs/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(ecb-options-version "2.40")
+ '(indicate-buffer-boundaries (quote right))
+ '(indicate-empty-lines t)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(user-mail-address "EMAIL@ADDRESS"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background nil :foreground nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(magit-item-highlight ((t nil))))
