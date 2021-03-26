160c0ac2ffcf244d7ac1268558a8a2e3d2b6a670
Jens Petersen <petersen@redhat.com>
2021-03-26 13:55:49 +0800

    latest emacs init.el
    - white space handling
    - modeline
    - git-ps1
    - projectile
    - eglot with flycheck
    - colored compile
    - undo-tree
    - git-gutter

--- emacs/init.el
+++ emacs/init.el
@@ -6,10 +6,10 @@
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
 
 (setq column-number-mode t
       custom-file "~/.emacs.d/custom.el"
+      delete-trailing-lines t
       diff-switches "-u"
       enable-recursive-minibuffers t
       frame-title-format "%b - emacs"
@@ -20,6 +20,7 @@
       kill-whole-line t
       save-place-file "~/.emacs.d/saveplaces.el"
       save-place-limit 1000
+      show-trailing-whitespace t
       show-paren-mode t
       size-indication-mode t
       tar-mode-show-date t
@@ -27,22 +28,38 @@
       use-dialog-box nil
       visible-bell t)
 
+;;; whitespace
+(add-hook 'before-save-hook
+          (lambda ()
+            (unless (eq major-mode 'diff-mode)
+              (delete-trailing-whitespace))))
+
+;;; mode-line: reduce whitespace
+(setq-default mode-line-format
+              '("%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification mode-line-buffer-identification "  " mode-line-position (vc-mode vc-mode) " "
+                mode-line-modes mode-line-misc-info mode-line-end-spaces)
+
+              mode-line-buffer-identification
+              '(#("%b" 0 2
+                  (mouse-face mode-line-highlight face mode-line-buffer-id))))
+
+(setq mode-line-frame-identification " " ; (:eval (mode-line-frame-control))
+      mode-line-front-space nil)
+
 (setq-default indent-tabs-mode nil
 	      indicate-buffer-boundaries 'left
-	      indicate-empty-lines t
-	      save-place t)
+	      indicate-empty-lines t)
+
+(save-place-mode t)
 
 (load custom-file)
 
 (global-set-key "\C-xw" 'write-region)
+(global-set-key "\C-cb" 'magit-checkout)
 (global-set-key "\C-ce" 'shell)
 (global-set-key "\C-cf" 'find-function-other-window)
 (global-set-key "\C-cg" 'magit-status)
-(global-set-key "\C-cj" '(lambda () (interactive)
-                           (let ((type (projectile-project-type)))
-                             (if (and type (not (eq type 'generic)))
-                                 (call-interactively 'projectile-compile-project)
-                               (call-interactively 'compile)))))
+(global-set-key "\C-cj" 'compile)
 (global-set-key "\C-ck" 'find-function-on-key)
 (global-set-key "\C-cl" 'magit-log-current)
 (global-set-key "\C-cq" 'bury-buffer)
@@ -104,7 +121,7 @@
 (define-key dired-mode-map "=p" 'dired-epatch)
 
 ;;; dired-x
-(setq dired-guess-shell-alist-user 
+(setq dired-guess-shell-alist-user
       '(("\\.pdf$" "evince")
 	("\\.tar.*$" "tar tvf" "tar xvf")
 	))
@@ -118,13 +135,6 @@
 (setq eshell-buffer-shorthand t
       eshell-hist-ignoredups t
       eshell-term-name "eterm-color")
-(defun juhp-eshell-hook-fn ()
-  (define-key eshell-mode-map "\C-u" 'eshell-kill-input)
-  (define-key eshell-mode-map "\C-w" 'backward-kill-word)
-  ;(setq pcomplete-cycle-completions nil)
-  )
-;(add-hook 'eshell-mode-hook
-;	  'juhp-eshell-hook-fn)
 
 (defun eshell/less (&rest args)
     "Invoke `view-file' on a file."
@@ -140,7 +150,6 @@
 (require 'iswitchb)
 (iswitchb-mode)
 (setq read-buffer-function 'iswitchb-read-buffer)
-;(iswitchb-default-keybindings)
 (defalias 'read-buffer 'iswitchb-read-buffer)
 
 ;;; w3m
@@ -163,10 +172,6 @@
 ;;; saveplace
 (require 'saveplace)
 
-;; scala
-;(add-to-list 'load-path "~/usr/scala/misc/scala-tool-support/emacs/")
-;(require 'scala-mode-auto)
-
 ;;; server
 (setenv "EDITOR" "emacsclient")
 (require 'server)
@@ -179,11 +184,6 @@
 ;;; ack
 (add-to-list 'load-path "~/.emacs.d/lisp/ShellArchive")
 (autoload 'ack "ack" nil t)
-
-;;; show-wspace
-;; (require 'show-wspace)
-;; (add-hook 'font-lock-mode-hook 'show-ws-highlight-tabs)
-;; (add-hook 'font-lock-mode-hook 'show-ws-highlight-trailing-whitespace)
 
 ;;; whitespace
 (setq-default whitespace-style '(tabs trailing space-before-tab newline indentation empty space-after-tab))
@@ -210,18 +210,101 @@
         (calendar-extract-month iso-date)
         (calendar-extract-year iso-date)))))
 
-;;; git
-;(add-to-list 'load-path "~/.emacs.d/lisp/git-emacs")
-;(require 'git-emacs-autoloads)
-
 ;;; magit
 (setq magit-omit-untracked-dir-contents t)
 (put 'scroll-left 'disabled nil)
-(autoload 'magit-display-log "magit" nil t)
 
 ;;; browse-url
 (setq browse-url-browser-function 'browse-url-xdg-open)
 
-;; Intero
-(package-install 'intero)
-;(add-hook 'haskell-mode-hook 'intero-mode)
+;;; git-ps1
+(let ((file "/usr/share/git-core/contrib/completion/git-prompt.sh"))
+  (when (file-exists-p file)
+    (setq git-ps1-mode-ps1-file file
+          vc-handled-backends '(Git))
+    (make-variable-buffer-local 'git-ps1-mode)
+    (add-hook 'dired-mode-hook 'git-ps1-mode)))
+
+(savehist-mode)
+
+;;; projectile
+(require 'projectile)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
+(defun projectile-my-mode-line ()
+  "Report project name and type in the modeline."
+  " Prj")
+(setq projectile-mode-line-function 'projectile-my-mode-line)
+
+; eglot
+(use-package eglot
+  :config
+  (add-to-list 'eglot-server-programs '(haskell-mode . ("haskell-language-server-wrapper" "--lsp"))))
+
+;;; use flycheck instead of flymake (to avoid process flood)
+(use-package flycheck)
+;; https://gist.github.com/purcell/ca33abbea9a98bb0f8a04d790a0cadcd
+    (defvar-local flycheck-eglot-current-errors nil)
+
+    (defun flycheck-eglot-report-fn (diags &rest _)
+      (setq flycheck-eglot-current-errors
+            (mapcar (lambda (diag)
+                      (save-excursion
+                        (goto-char (flymake--diag-beg diag))
+                        (flycheck-error-new-at (line-number-at-pos)
+                                               (1+ (- (point) (line-beginning-position)))
+                                               (pcase (flymake--diag-type diag)
+                                                 ('eglot-error 'error)
+                                                 ('eglot-warning 'warning)
+                                                 ('eglot-note 'info)
+                                                 (_ (error "Unknown diag type, %S" diag)))
+                                               (flymake--diag-text diag)
+                                               :checker 'eglot)))
+                    diags))
+      (flycheck-buffer))
+
+    (defun flycheck-eglot--start (checker callback)
+      (funcall callback 'finished flycheck-eglot-current-errors))
+
+    (defun flycheck-eglot--available-p ()
+      (bound-and-true-p eglot--managed-mode))
+
+    (flycheck-define-generic-checker 'eglot
+      "Report `eglot' diagnostics using `flycheck'."
+      :start #'flycheck-eglot--start
+      :predicate #'flycheck-eglot--available-p
+      :modes '(prog-mode text-mode))
+
+    (push 'eglot flycheck-checkers)
+
+    (defun sanityinc/eglot-prefer-flycheck ()
+      (when eglot--managed-mode
+        (flycheck-add-mode 'eglot major-mode)
+        (flycheck-select-checker 'eglot)
+        (flycheck-mode)
+        (flymake-mode -1)
+        (setq eglot--current-flymake-report-fn 'flycheck-eglot-report-fn)))
+
+    (add-hook 'eglot--managed-mode-hook 'sanityinc/eglot-prefer-flycheck)
+
+;; grep
+(eval-after-load "grep"
+  '(progn
+    (add-to-list 'grep-find-ignored-files "*_flymake.*")
+    (add-to-list 'grep-find-ignored-directories "dist")
+    (add-to-list 'grep-find-ignored-directories "dist-newstyle")
+    ))
+
+;;; undo-tree
+(global-undo-tree-mode)
+
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (when (eq major-mode 'compilation-mode)
+    (ansi-color-process-output nil)
+    (setq-local comint-last-output-start (point-marker))))
+(add-hook 'compilation-filter-hook
+          #'colorize-compilation-buffer)
+
+;;; git-gutter
+(global-git-gutter-mode +1)
