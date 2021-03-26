5be8b9941b91dd06b3393e06efa147c30b324db3
Jens Petersen <petersen@redhat.com>
2021-03-26 13:55:36 +0800

    sync emacs custom.el

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,9 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(column-number-mode t)
- '(indicate-buffer-boundaries (quote right))
+ '(fci-rule-color "#383838")
+ '(frame-background-mode 'dark)
+ '(git-ps1-mode-lighter-text-format " [%s]")
+ '(indicate-buffer-boundaries 'right)
  '(indicate-empty-lines t)
+ '(magit-diff-options nil)
+ '(magit-log-margin '(t "%Y-%m-%d %H:%M " magit-log-margin-width t 18))
+ '(main-line-color1 "#222232")
+ '(main-line-color2 "#333343")
  '(show-paren-mode t)
  '(size-indication-mode t)
  '(user-mail-address "EMAIL@ADDRESS"))
@@ -14,5 +25,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background nil :foreground nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
- '(magit-item-highlight ((t nil))))
+ '(agda2-highlight-datatype-face ((t (:foreground "dodger blue"))))
+ '(agda2-highlight-function-face ((t (:foreground "royal blue"))))
+ '(agda2-highlight-primitive-face ((t (:foreground "cornflower blue"))))
+ '(font-lock-comment-face ((t (:foreground "chocolate2"))))
+ '(fringe ((t (:background "grey25"))))
+ '(magit-item-highlight ((t nil)))
+ '(minibuffer-prompt ((t nil)))
+ '(rpm-spec-tag-face ((t (:foreground "lightblue")))))
